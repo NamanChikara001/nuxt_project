de0f0d65a8ae9725ac89b05a0649a5ed830f6cab
NamanChikara001 <79321316+NamanChikara001@users.noreply.github.com>
2025-08-25 13:04:36 +0530

    Update posts.js

--- my-vue-app/src/stores/posts.js
+++ my-vue-app/src/stores/posts.js
@@ -68,17 +68,35 @@ export const usePostsStore = defineStore('posts', {
     error: null
   }),
   actions: {
-    async fetchPosts() {
-      this.loading = true
-      try {
-        
-        const res = await axios.get('https://dummyjson.com/posts')
-        this.posts = res.data.posts
-      } catch (err) {
-        this.error = err.message
-      } finally {
-        this.loading = false
-      }
-    }
+  async fetchPosts() {
+  const cacheKey = 'cached_posts'
+  const cacheTimestampKey = 'cachedPostsTimestamp'
+  const now = Date.now()
+
+  const cachedData = localStorage.getItem(cacheKey)
+  const cachedTimestamp = localStorage.getItem(cacheTimestampKey)
+
+  if (cachedData && cachedTimestamp && now - Number(cachedTimestamp) < 15 * 60 * 1000) {
+    this.posts = JSON.parse(cachedData)
+    return
+  }
+
+  this.loading = true
+  try {
+    const res = await axios.get('https://dummyjson.com/posts')
+    this.posts = res.data.posts
+
+    // Store in localStorage
+    localStorage.setItem(cacheKey, JSON.stringify(this.posts))
+    localStorage.setItem(cacheTimestampKey, now.toString())
+  } catch (err) {
+    this.error = err.message
+  } finally {
+    this.loading = false
+  }
+}
+
   }
 })
+
+
